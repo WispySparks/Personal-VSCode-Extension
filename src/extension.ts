3a656cdf4dc8dfd149f23d0248d89ce7bd25eb71
WispySparks <wispysparks@gmail.com>
2022-08-01 14:19:52 -0500

    create folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,54 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import { Uri } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "java-project-template" is now active!');
-
+	const dialogOptions: vscode.OpenDialogOptions = {
+		canSelectMany: false,
+		openLabel: 'Select Project Location',
+		canSelectFiles: false,
+		canSelectFolders: true
+	};
+	const inputOptions: vscode.InputBoxOptions = {
+		prompt: "Input a Project Name",
+		validateInput(value) {
+			if (value.length <= 0) {
+				return "Must give a Name";
+			}
+			else {
+				return "";
+			}
+		},
+	}
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('java-project-template.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Java Project Template!');
+	let path: Uri["fsPath"];
+	let name: string;
+	let disposable = vscode.commands.registerCommand('java-project-template.createProject', () => {
+		vscode.window.showInformationMessage('Command Run');
+		vscode.window.showOpenDialog(dialogOptions).then(fileUri => {
+			if (fileUri != null) {
+				vscode.window.showInformationMessage(fileUri[0].fsPath);
+				path = fileUri[0].fsPath;
+				vscode.window.showInputBox(inputOptions).then(input => {
+					if (input != null) {
+						vscode.window.showInformationMessage(input);
+						name = input;
+						if (!fs.existsSync(path + "\\" + name)) {
+							fs.mkdirSync(path + "\\" + name);
+							vscode.commands.executeCommand('vscode.openFolder', Uri.file(path + "\\" + name))
+						}
+						else {
+							vscode.window.showErrorMessage("Already a Directory with that Name.");
+						}
+					}
+				});
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
