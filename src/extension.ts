210d0bbf3304fa97e602144e247e707389dc226a
WispySparks <wispysparks@gmail.com>
2022-08-02 15:45:10 -0500

    better validate input, cleaner code

--- src/extension.ts
+++ src/extension.ts
@@ -18,40 +18,36 @@ export function activate(context: vscode.ExtensionContext) {
 		prompt: "Input a Project Name",
 		validateInput(value) {
 			if (value.length <= 0) {
-				return "Must give a Name";
+				return "Must give a name";
+			}
+			else if (value.match("[\\\\/:*?\"<>|]") != null) {
+				return "Illegal Characters";
+			}
+			else if (fs.pathExistsSync(path.join(projectPath, value)) && value.length > 0) {
+				return "Directory already exists";
 			}
 			else {
 				return "";
 			}
 		},
 	}
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('java-project-template.createProject', () => {
 		vscode.window.showOpenDialog(dialogOptions).then(fileUri => {
-			if (fileUri != null) {
-				projectPath = fileUri[0].fsPath;
-				vscode.window.showInputBox(inputOptions).then(input => {
-					if (input != null) {
-						projectName = input;
-						let projectBase: string = path.join(projectPath, projectName);
-						if (!fs.pathExistsSync(projectBase)) {
-							fs.ensureDirSync(projectBase);
-							fs.copySync(templatePath, projectBase);
-							fs.ensureDirSync(path.join(projectBase, 'src', projectName));
-							fs.copyFileSync(path.join(context.extensionPath, 'templates', 'Main.java'), path.join(projectBase, 'src', projectName, 'Main.java'));
-							let file = fs.readFileSync(path.join(projectBase, 'src', projectName, 'Main.java'));
-							fs.writeFileSync(path.join(projectBase, 'src', projectName, 'Main.java'), ("package " + projectName + ";\n" + file.toString()));
-							const openNewWindow: boolean = vscode.workspace.workspaceFolders != undefined;
-							vscode.commands.executeCommand('vscode.openFolder', Uri.file(projectBase), openNewWindow);
-						}
-						else {
-							vscode.window.showErrorMessage("Already a Directory with that Name.");
-						}
-					}
-				});
-			}
+			if (fileUri == undefined) return;
+			projectPath = fileUri[0].fsPath;
+			vscode.window.showInputBox(inputOptions).then(input => {
+				if (input == undefined) return;
+				projectName = input;
+				let projectBase: string = path.join(projectPath, projectName);
+				fs.ensureDirSync(projectBase);
+				fs.copySync(templatePath, projectBase);
+				fs.ensureDirSync(path.join(projectBase, 'src', projectName));
+				fs.copyFileSync(path.join(context.extensionPath, 'templates', 'Main.java'), path.join(projectBase, 'src', projectName, 'Main.java'));
+				let file = fs.readFileSync(path.join(projectBase, 'src', projectName, 'Main.java'));
+				fs.writeFileSync(path.join(projectBase, 'src', projectName, 'Main.java'), ("package " + projectName + ";\n" + file.toString()));
+				const openNewWindow: boolean = vscode.workspace.workspaceFolders != undefined;
+				vscode.commands.executeCommand('vscode.openFolder', Uri.file(projectBase), openNewWindow);
+			});
 		});
 	});
 
